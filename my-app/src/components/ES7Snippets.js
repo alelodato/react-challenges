b4e09e76234b6a5029e67e70b81de4fba248956d
Alessio Lodato <swly7j4e@students.codeinstitute.net>
2025-02-20 15:07:17 +0000

    added comments for most common used react snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,60 @@
+// Most common React Snippet
+
+// rfce: creates a React Functional Component
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rafce: creates a React Arrow Function Component
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rce: creates a React class component
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// rconst: adds a default constructor for 
+// a class that contains 
+// props and arguments
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// clg: console.log(object)
+
+// console.log(first)
+
+// clo: console.log('object',object)
+
+// console.log('first', first)
